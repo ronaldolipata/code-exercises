4936e65d8685993d5bc8273a7a18d1e0a3b9b102
Ronaldo Lipata <lipata.ronaldo@gmail.com>
2023-09-30 00:40:49 +0800

    refac: code and comments

--- javascript/fizzBuzz.js
+++ javascript/fizzBuzz.js
@@ -1,25 +1,31 @@
+/**
+ * Function to perform the FizzBuzz game.
+ * @param {number} number - The number to evaluate.
+ * @returns {string | number} - Returns 'Fizz' if divisible by 3, 'Buzz' if divisible by 5,
+ * 'FizzBuzz' if divisible by both 3 and 5, or the number itself otherwise.
+ */
 function fizzBuzz(number) {
   if (number % 15 === 0) {
-    // Print "FizzBuzz" if the number is divisible by 3 and 5
+    // If the number is divisible by both 3 and 5, print 'FizzBuzz'
     console.log('FizzBuzz');
+    return 'FizzBuzz';
   } else if (number % 3 === 0) {
-    // Print "Buzz" if the number is divisible by 3
-    console.log('Buzz');
-  } else if (number % 5 === 0) {
-    // Print "Fizz" if the is divisible by 5
+    // If the number is divisible by 3, print 'Fizz'
     console.log('Fizz');
+    return 'Fizz';
+  } else if (number % 5 === 0) {
+    // If the number is divisible by 5, print 'Buzz'
+    console.log('Buzz');
+    return 'Buzz';
   } else {
-    // Print the number if the above conditions are not met
+    // If none of the conditions are met, print the number itself
     console.log(number);
+    return number;
   }
-
-  return number;
 }
 
-const number = 100;
+const limit = 100; // Define the limit for FizzBuzz
 
-for (let i = 1; i < number; i++) {
-  fizzBuzz(i);
+for (let i = 1; i <= limit; i++) {
+  fizzBuzz(i); // Call the fizzBuzz function for numbers from 1 to 'limit'
 }
-
-console.log(fizzBuzz(number));
